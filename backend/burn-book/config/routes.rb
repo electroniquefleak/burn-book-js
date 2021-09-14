6be76d61f5f1c6c18ee28522b6ce185c2ba1cd6c
Hunter <fleakhunter@gmail.com>
2021-09-13 23:28:25 -0400

    Create Comment resource; rcorrect routes.rb.

--- backend/burn-book/config/routes.rb
+++ backend/burn-book/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :confessions
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :comments, only: [:create]
+  resources :confessions, only: [:index, :create, :show]
 end
