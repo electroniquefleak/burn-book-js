49506096ad9b4d800a5d8cb98e48d52ecbcff60d
Hunter <fleakhunter@gmail.com>
2021-09-13 23:18:11 -0400

    Create Confessions resource.

---
+++ backend/burn-book/db/migrate/20210914031758_create_confessions.rb
@@ -0,0 +1,10 @@
+class CreateConfessions < ActiveRecord::Migration[6.1]
+  def change
+    create_table :confessions do |t|
+      t.string :title
+      t.string :body
+
+      t.timestamps
+    end
+  end
+end
