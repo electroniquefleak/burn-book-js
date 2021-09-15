3d3d04c331dc3bdf94f538016ddd2e4584a1707d
Hunter <fleakhunter@gmail.com>
2021-09-14 23:15:52 -0400

    Add Confessions show for API endpoint.

--- backend/burn-book/app/controllers/confessions_controller.rb
+++ backend/burn-book/app/controllers/confessions_controller.rb
@@ -3,4 +3,9 @@ class ConfessionsController < ApplicationController
         @confessions = Confession.all
         render json: @confessions
     end
+
+    def show
+        @confession = Confession.find(params[:id])
+        render json: @confession.as_json(include: :comments)
+    end
 end
