51b0f9a12cf686e9248cd60fc6a4fb7ef273baed
Hunter <fleakhunter@gmail.com>
2021-09-19 22:14:46 -0400

    Add Create action on Confession; add params.

--- backend/burn-book/app/controllers/confessions_controller.rb
+++ backend/burn-book/app/controllers/confessions_controller.rb
@@ -8,4 +8,20 @@ class ConfessionsController < ApplicationController
         @confession = Confession.find(params[:id])
         render json: @confession.as_json(include: :comments)
     end
+
+    def create
+        @confession = Confession.new(confession_params)
+        if @confession.save
+            puts "Confession created!"
+            render json: @confession
+        else
+            puts "Error: failed to create confession."
+        end
+    end
+
+    private
+
+    def confession_params
+        params.require(:confession).permit(:title, :body)
+    end
 end
