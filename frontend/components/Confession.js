8e479f8ec5fd3f357ee35dc70dc723785d2d25d7
Hunter <fleakhunter@gmail.com>
2021-09-23 00:57:13 -0400

    Add Confession.js script on index.html; add getAllConfessions method in Confeesion class.

--- frontend/components/Confession.js
+++ frontend/components/Confession.js
@@ -1,10 +1,20 @@
 class Confession {
 
-    static allConfessions;
+    static allConfessions = [];
 
     constructor(data){
         this.data = data
         this.comments = this.data.comments.map(comment => new Comment(comment, this))
         this.constructor.allConfessions.push(this)
+    }
+
+    static getAllConfessions = () => {
+        api.getAllConfessions()
+            .then(confessions => {
+                Confession.allConfessions = [];
+                confessions.array.forEach(confession => new Confession(confession));
+                this.renderConfessions();
+            })
+            .catch(error => console.error(error))
     }
 }
