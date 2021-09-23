55bc0068b366e5bd30dca50e6379ba5da272599f
Hunter <fleakhunter@gmail.com>
2021-09-22 23:55:24 -0400

    CSS style changes; add creation function for Comments Controller.

--- backend/burn-book/app/controllers/comments_controller.rb
+++ backend/burn-book/app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.confession_id = params[:confession_id]
+        if @comment.save
+            puts "Comment added!"
+            render json: @confession
+        else
+            puts "Error: failed to create comment."
+        end
+    end
+
+    private
+
+    def comment_params
+        params.require(:confession).permit(:body, :confession_id)
+    end
+
 end
